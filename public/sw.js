046d1afadb8df70a17ed695915893c1a2412b177
sonam <sonampriya404@gmail.com>
2017-11-06 17:44:32 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -87,7 +87,7 @@ self.addEventListener('push', function(event) {
 
 self.addEventListener('notificationclick', function(event) {
 
-  var url = './latest.html';
+  var url = 'https://github.com/sonampriya/pwa-apis/commits/master';
 
   event.notification.close(); //Close the notification
 
